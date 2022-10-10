9c8ce4ce228be3478ad502d3f124c8775df00d53
konradsawicki <konradsawicki98@gmail.com>
2022-10-10 19:38:39 +0200

    added new features

--- script.sh
+++ script.sh
@@ -13,10 +13,17 @@ menu()
 
 get_country_info()
 {
-    COUNTRY_POPULATION=`grep cp1\" $COUNTRY_COUNTRY_FILE | cut -d '>' -f 4`
+    COUNTRY_POPULATION=`grep cp1\" $COUNTRY_INFO_FILE | cut -d '>' -f 4`
     COUNTRY_POPULATION=${COUNTRY_POPULATION%</div}
 
+    COUNTRY_BIRTHS=`grep cp7\" $COUNTRY_INFO_FILE | cut -d '>' -f 4`
+    COUNTRY_BIRTHS=${COUNTRY_BIRTHS%</div}
 
+    COUNTRY_DEATHS=`grep cp9\" $COUNTRY_INFO_FILE | cut -d '>' -f 4`
+    COUNTRY_DEATHS=${COUNTRY_DEATHS%</div}
+
+    COUNTRY_GROWTH=`grep cp12\" $COUNTRY_INFO_FILE | cut -d '>' -f 4`
+    COUNTRY_GROWTH=${COUNTRY_GROWTH%</div}
 }
 
 choose_country()
@@ -35,6 +42,8 @@ choose_country()
 
     update_country_info
     get_country_info
+    print_country_info
+    FOUND_COUNTRY="FALSE"
 }
 
 change_coutry_to_fit_pattern() 
@@ -55,13 +64,22 @@ change_coutry_to_fit_pattern()
 
 check_country_on_the_list()
 {
-    grep $COUNTRY "$COUNTRY_LIST_FILE"
+    grep -q $COUNTRY "$COUNTRY_LIST_FILE"
     [[ $? -eq 0 ]] && FOUND_COUNTRY="TRUE" || FOUND_COUNTRY="FALSE"
 }
 
 update_country_info()
 {
     wget -O "$COUNTRY_INFO_FILE" "$MAIN_URL"/"$COUNTRY"
+}
+
+print_country_info()
+{
+    COUNTRY=`echo $COUNTRY | tr '_' ' '`
+    zenity --info --title "Statistics for: $COUNTRY" --height 150 --width 300\
+        --text "\nCurrent population: $COUNTRY_POPULATION\
+            \nBirths today: $COUNTRY_BIRTHS\nDeaths today: $COUNTRY_DEATHS\
+            \nPopulation growth year to date: $COUNTRY_GROWTH"
 }
 
 get_country_list()
@@ -75,7 +93,7 @@ delete_files()
     rm /tmp/country_list.$$
 }
 
-MAIN_URL="https:://countrymeters.info/en"
+MAIN_URL="https://countrymeters.info/en"
 
 COUNTRY_INFO_FILE="/tmp/country_info.$$"
 COUNTRY_LIST_FILE="/tmp/country_list.$$"
@@ -85,11 +103,13 @@ COUNTRY="NONE"
 COUNTRY_POPULATION=0
 COUNTRY_BIRTHS=0
 COUNTRY_DEATHS=0
+COUNTRY_GROWTH=0
 
 MENU_LIST=("Choose specific country population statistics" "Exit")
 MENU=","
 
 get_country_list
 menu
+delete_files
 
 exit 0 
